93dab59d98de93ae42369931e39f624581c2084b
mexak <a_urbanska@hotmail.com>
2018-05-14 11:35:21 +0200

    Models minor changes

--- bitcoin/bitcoin_dc/models.py
+++ bitcoin/bitcoin_dc/models.py
@@ -9,15 +9,10 @@ class Address(models.Model):
 
 
 class Transactions(models.Model):
-    # address = models.ForeignKey(Address, on_delete=models.CASCADE)
-    # tid = models.CharField(max_length=64, default='tid')
-    # date = models.PositiveIntegerField(default=1)
-    # amount = models.PositiveIntegerField(default=1)
-
     address = models.ForeignKey(Address, on_delete=models.CASCADE)
     tx_index = models.CharField(max_length=64, default='tid')
     time = models.PositiveIntegerField(default=1)
     size = models.PositiveIntegerField(default=1)
 
     def __str__(self):
-        return self.tid
+        return self.tx_index
